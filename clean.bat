4d446e85ddcd5af7dbe005a46138870f74393d8b
Christian Mulser <christianmulser000@gmail.com>
2021-11-01 19:38:36 +0100

    Added cleaning scripts

---
+++ clean.bat
@@ -0,0 +1,4 @@
+del /s /q game\bin\win32
+del /s /q mete\bin\win32
+del /s /q mete\lib\win32
+del /s /q game\out\win32
---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf game/bin/linux
+rm -rf mete/bin/linux
+rm -rf mete/lib/linux
+rm -rf game/out/linux
