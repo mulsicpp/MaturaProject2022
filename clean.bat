72738aa31dde43b74dcde67fa89786d9a5006ad7
Christian Mulser <christianmulser000@gmail.com>
2021-11-02 10:31:15 +0100

    Updated clean scripts and changed .gitignore

--- clean.bat
+++ clean.bat
@@ -1,5 +1,5 @@
 del /s /q game\bin\win32
 del /s /q gear\bin\win32
 del /s /q gear\lib\win32
-del /s /q gear\external\glad\win32
+del /s /q gear\external\glad\bin\win32
 del /s /q game\out\win32
--- clean.sh
+++ clean.sh
@@ -3,5 +3,5 @@
 rm -rf game/bin/linux
 rm -rf gear/bin/linux
 rm -rf gear/lib/linux
-rm -rf gear/external/glad/linux
+rm -rf gear/external/glad/bin/linux
 rm -rf game/out/linux
