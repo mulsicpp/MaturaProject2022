060d072e36d2c394463964d90f257537ed10ff13
Christian Mulser <christianmulser000@gmail.com>
2022-03-23 12:22:33 +0100

    Extended animation component

--- gear/src/gear/renderer/AnimationComponent.h
+++ gear/src/gear/renderer/AnimationComponent.h
@@ -4,16 +4,23 @@
 #include <gear/math/Vector.h>
 #include <gear/resource/Animation.h>
 #include <gear/resource/Palette.h>
+#include <functional>
+
+#include "AnimationEvent.h"
 
 _GEAR_START
 
 struct AnimationComponent {
   Ref<Animation> animation;
   Ref<Palette> palette;
-  float animation_Offset;
+  AnimationType type = AnimationType::LOOP;
+  float frame_Offset;
   float frame_Rate;
   float parallax_Factor = 1;
   gear::Vector<float, 3> offset = { 0, 0, 0 };
+
+  std::function<void (AnimationEvent)> on_Change;
+  std::function<void (AnimationEvent)> on_End;
 };
 
 _GEAR_END
---
+++ gear/src/gear/renderer/AnimationType.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <gear/core/core.h>
+
+_GEAR_START
+
+enum class AnimationType {
+    LOOP,
+    FORWARD,
+    PING_PONG
+};
+
+_GEAR_END
