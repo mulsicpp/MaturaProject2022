55f6b1ed7d363e6722727c678d05dc9992c93a28
Christian Mulser <christianmulser000@gmail.com>
2022-03-26 16:49:15 +0100

    Changed animation events

--- gear/src/gear/renderer/animation_player.cpp
+++ gear/src/gear/renderer/animation_player.cpp
@@ -7,21 +7,29 @@ static std::vector<std::function<void(void)>> callbacks;
 
 void gear::animation_Player_Callback(gear::AnimationComponent &animation)
 {
+    int prev_Frame = animation.frame_Offset;
     switch (animation.type)
     {
     case AnimationType::FORWARD:
         if (animation.frame_Offset + animation.frame_Rate * gear::Game::get_Delta_Time() < animation.animation->get_Frame_Count())
             animation.frame_Offset += animation.frame_Rate * gear::Game::get_Delta_Time();
-        else
-            if(animation.on_End != nullptr) callbacks.push_back(std::bind(animation.on_End, AnimationEvent(animation.frame_Offset, animation.frame_Rate, animation.type)));
-        if(animation.on_Change != nullptr) callbacks.push_back(std::bind(animation.on_Change, AnimationEvent(animation.frame_Offset, animation.frame_Rate, animation.type)));
+        else if (animation.on_Ended != nullptr)
+            callbacks.push_back(animation.on_Ended);
+        if (animation.on_Changed != nullptr)
+            callbacks.push_back(std::bind(animation.on_Changed, animation.frame_Offset));
+        if (animation.on_Frame_Changed != nullptr && (int)animation.frame_Offset != prev_Frame)
+            callbacks.push_back(std::bind(animation.on_Frame_Changed, (int)animation.frame_Offset));
         break;
     case AnimationType::LOOP:
         if (animation.frame_Offset + animation.frame_Rate * gear::Game::get_Delta_Time() >= animation.animation->get_Frame_Count())
-            if(animation.on_End != nullptr) callbacks.push_back(std::bind(animation.on_End, AnimationEvent(animation.frame_Offset, animation.frame_Rate, animation.type)));
+            if (animation.on_Ended != nullptr)
+                callbacks.push_back(animation.on_Ended);
         animation.frame_Offset += animation.frame_Rate * gear::Game::get_Delta_Time();
         animation.frame_Offset = int(animation.frame_Offset) % animation.animation->get_Frame_Count() + animation.frame_Offset - int(animation.frame_Offset);
-        if(animation.on_Change != nullptr) callbacks.push_back(std::bind(animation.on_Change, AnimationEvent(animation.frame_Offset, animation.frame_Rate, animation.type)));
+        if (animation.on_Changed != nullptr)
+            callbacks.push_back(std::bind(animation.on_Changed, animation.frame_Offset));
+        if (animation.on_Frame_Changed != nullptr && (int)animation.frame_Offset != prev_Frame)
+            callbacks.push_back(std::bind(animation.on_Frame_Changed, (int)animation.frame_Offset));
         break;
     case AnimationType::PING_PONG:
         animation.frame_Offset += animation.frame_Rate * gear::Game::get_Delta_Time() * animation.factor;
@@ -31,16 +39,21 @@ void gear::animation_Player_Callback(gear::AnimationComponent &animation)
             {
                 animation.frame_Offset = 2 * animation.animation->get_Frame_Count() - animation.frame_Offset;
                 animation.factor = -1;
-                if(animation.on_End != nullptr) callbacks.push_back(std::bind(animation.on_End, AnimationEvent(animation.frame_Offset, animation.frame_Rate, animation.type)));
+                if (animation.on_Ended != nullptr)
+                    callbacks.push_back(animation.on_Ended);
             }
             if (animation.frame_Offset < 0)
             {
                 animation.frame_Offset = -animation.frame_Offset;
                 animation.factor = 1;
-                if(animation.on_End != nullptr) callbacks.push_back(std::bind(animation.on_End, AnimationEvent(animation.frame_Offset, animation.frame_Rate, animation.type)));
+                if (animation.on_Ended != nullptr)
+                    callbacks.push_back(animation.on_Ended);
             }
         }
-        if(animation.on_Change != nullptr) callbacks.push_back(std::bind(animation.on_Change, AnimationEvent(animation.frame_Offset, animation.frame_Rate, animation.type)));
+        if (animation.on_Changed != nullptr)
+            callbacks.push_back(std::bind(animation.on_Changed, animation.frame_Offset));
+        if (animation.on_Frame_Changed != nullptr && (int)animation.frame_Offset != prev_Frame)
+            callbacks.push_back(std::bind(animation.on_Frame_Changed, (int)animation.frame_Offset));
         break;
     }
 }
@@ -48,7 +61,7 @@ void gear::animation_Player_Callback(gear::AnimationComponent &animation)
 void gear::continue_Animations(gear::Scene *scene)
 {
     gear::Entity::for_Each(scene->get_ID(), gear::animation_Player_Callback);
-    for(auto &callback : callbacks)
+    for (auto &callback : callbacks)
         callback();
     callbacks.clear();
 }
